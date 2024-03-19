db976b0bf24558d9dbc917441127fb6d3f1c85f0
mrolls13 <matthewrolls13@gmail.com>
2024-03-19 13:26:42 -0600

    added unit test and updated readme

--- problems/fizzbuzz/cpp/matthewrolls/README.md
+++ problems/fizzbuzz/cpp/matthewrolls/README.md
@@ -2,6 +2,8 @@
 
 To run the fizzbuzz solution simply enter ./script.sh into your shell which will then compile fzbz-mr.cpp into the executable fzbz-mr and will run it. You will then be prompted for a number. Simply enter a number greater than 0 and the program will run and solve fizzbuzz up until that number.
 
+In order to test the program simply enter an integer less than 0 on the commandline.
+
 Note: Make sure to enter chmod +x script.sh beforehand to ensure it has the neccessary permissions to run properly.
 
 ```
--- problems/fizzbuzz/cpp/matthewrolls/fzbz-mr.cpp
+++ problems/fizzbuzz/cpp/matthewrolls/fzbz-mr.cpp
@@ -1,26 +1,39 @@
 #include <iostream>
 #include <stdexcept>
 #include <stdlib.h>
+#include <string>
+#include <cassert>
 using namespace std;
-//enter number at end of run command to get input
-void error(string in1){
-	throw runtime_error("Error: "+in1);
+
+string fb(int inp){
+	string s;
+	for(int k=1; k<=inp; k++){
+		if(k%3==0 && k%5==0){
+			s.append("FizzBuzz\n");
+		}else if(k%3==0){
+			s.append("Fizz\n");
+		}else if(k%5==0){
+			s.append("Buzz\n");	
+		}else{
+			s.append(to_string(k));
+			s.append("\n");	
+		}
+	}
+	return s;
 }
 int main(int argc, char** argv){
-	if(argc<2){
-		error("No input");
-	}
 	int i=atoi(argv[1]);
-	for(int k=1; k<=i; k++){
-		if(k%3==0 && k%5==0){
-			cout<<"FizzBuzz"<<"\n";
-		}else if(k%3==0){
-			cout<<"Fizz"<<"\n";
-		}else if(k%5==0){
-			cout<<"Buzz"<<"\n";	
-		}else{
-			cout<<k<<"\n";	
-		}
+	if(i>0){
+		string out = fb(i);
+		cout<<out;
+	}else{
+		cout<<"Testing\n";
+		assert("1\n2\nFizz\n"==fb(3));
+		cout<<"First test passed\n";
+		assert("1\n2\nFizz\n4\nBuzz\n"==fb(5));
+		cout<<"Second test passed\n";
+		assert("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n"==fb(15));
+		cout<<"Third test passed\n";
 	}
 	return(0);
 }
