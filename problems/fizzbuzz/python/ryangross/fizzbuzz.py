d61e68901fff417d51aa16c85349295206c82371
ryang <ryangrossmn@gmail.com>
2024-03-19 14:54:15 -0600

    created test.py and run_tests.sh, deleted a duplicate index.html

--- problems/fizzbuzz/python/ryangross/fizzbuzz.py
+++ problems/fizzbuzz/python/ryangross/fizzbuzz.py
@@ -1,15 +1,32 @@
 import sys
 
 def fizzbuzz(n):
+    val = "0"
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
+            val = "FizzBuzz"
             print('FizzBuzz')
+            # return val
+
         elif i % 3 == 0:
+            val = "Fizz"
             print('Fizz')
+            # return val
+
         elif i % 5 == 0:
+            val = "Buzz"
             print('Buzz')
+            # return val
+
         else:
+            val = str(i)
             print(i)
+            # return val
+    
+    # print(val)
+    return val
+    
+        # val is for testing 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
