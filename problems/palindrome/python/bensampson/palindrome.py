4f4404f8d418fd26a55476b457b60a9f60dca818
Bennett Sampson <sampson246156@gmail.com>
2024-03-26 14:47:54 -0600

    Putting user input back for future assignment

--- problems/palindrome/python/bensampson/palindrome.py
+++ problems/palindrome/python/bensampson/palindrome.py
@@ -1,19 +1,17 @@
 import sys
 
-inputString = "racecar"
 
 def isPalindrome(word):
     if word == "":
         return True
     for i in range(0, int(len(word)/2)):
         if word[i] != word[len(word)-i-1]:
-            print("The string " + word + " is NOT a palindrome!")
             return False
         else:
-            print("The string " + word + " is a palindrome!")
             return True
 
 def main():
+    inputString = sys.argv[1]
     
     if (isPalindrome(inputString)):
         print("The string " + inputString + " is a palindrome!")
@@ -21,4 +19,4 @@ def main():
         print("The string " + inputString + " is NOT a palindrome!")
         
 
-isPalindrome(inputString)
+main()
