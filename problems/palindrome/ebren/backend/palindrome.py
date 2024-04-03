de0acbb0d886128bb112d10f7f774d3a871698f0
Yabets Ebren <unosjabez@gmail.com>
2024-04-03 13:13:32 -0600

    style personal page and add palindrome form

---
+++ problems/palindrome/ebren/backend/palindrome.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder='../frontend')
+
+def is_palindrome(s):
+    cleanString = ''.join(char.lower() for char in s if char.isalnum())
+    if len(cleanString) <= 1:
+        return True
+    left = 0
+    right = len(cleanString) - 1
+    while left < right:
+        if cleanString[left] != cleanString[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/check-palindrome', methods=['POST'])
+def check_palindrome():
+    data = request.get_json()
+    try:
+        palindrome = data['palindrome']
+        if palindrome is not None:
+            if is_palindrome(palindrome):
+                return "true"
+            else:
+                return "false"
+        else:
+            return "Invalid input"
+    except Exception as e:
+        return f"The error: {str(e)}"
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ problems/palindrome/ebren/backend/runTest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This script runs the unit tests for the palindrome check program
+
+echo "Running unit tests..."
+python3 unitTest.py
