2df75d110c3c6dd9f1face86a312cf155f8cf28d
ryang <ryangrossmn@gmail.com>
2024-04-09 15:50:42 -0600

    yeah maybe

--- problems/fizzbuzz/python/ryangross/backend.py
+++ problems/fizzbuzz/python/ryangross/backend.py
@@ -1,13 +1,32 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template_string, render_template
 import fizzbuzz as fb
 app = Flask(__name__)
 
-@app.route('/index', methods=['GET', 'POST'])
-def wbapp():
-    if request.method == 'POST':
-        number = int(request.form['number'])
-        result = fb.fizzBuzz(number)[1]
-        return render_template_string("{{ result }}", result=result)
+# @app.route('/index', methods=['GET', 'POST'])
+# def wbapp():
+#     if request.method == 'POST':
+#         number = int(request.form['number'])
+#         result = fb.fizzBuzz(number)[1]
+#         return render_template_string("{{ result }}", result=result)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/index', methods=['POST'])
+def fizz_buzz_app():
+    number_str = request.form.get('number')
+    if number_str and number_str.isdigit():  # make sure its not empty and valid number
+        n = int(number_str)
+        result = numbers(n)
+    else:
+        result = "Please enter a valid number."
+    return render_template('fizzbuzz_app.html', result=result, number=number_str)
+
+def numbers(n):
+    result = fb.fizzbuzz(n)
+    return ''.join(map(str, result)) 
+
 
 if __name__ == '__main__':
     app.run()
