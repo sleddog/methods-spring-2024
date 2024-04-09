a0360a430d14f1e04e32fcb910f0e56f8227c26b
ryang <ryangrossmn@gmail.com>
2024-04-09 12:52:27 -0600

    added web functionality and updated tests to work with new format

--- problems/fizzbuzz/python/ryangross/fizzbuzz.py
+++ problems/fizzbuzz/python/ryangross/fizzbuzz.py
@@ -1,30 +1,42 @@
 import sys
+from flask import Flask, request, render_template_string
 
+app = Flask(__name__)
+
+
+@app.route('/fizzbuzz.py', methods=['GET'])
 def fizzbuzz(n):
+    number = int(request.args.get('number'))
+    result = ""
     val = "0"
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
-            val = "FizzBuzz"
-            print('FizzBuzz')
+            val = "FizzBuzz "
+            result += val
+            print('FizzBuzz ')
             # return val
 
         elif i % 3 == 0:
-            val = "Fizz"
-            print('Fizz')
+            val = "Fizz "
+            result += val
+            print('Fizz ')
             # return val
 
         elif i % 5 == 0:
-            val = "Buzz"
-            print('Buzz')
+            val = "Buzz "
+            result += val
+            print('Buzz ')
             # return val
 
         else:
-            val = str(i)
+            val = str(i) + " "
+            result += val
             print(i)
             # return val
     
     # print(val)
-    return val
+    # return val
+    return render_template_string("{{ result }}", result=result), val
     
         # val is for testing 
 
--- problems/fizzbuzz/python/ryangross/test.py
+++ problems/fizzbuzz/python/ryangross/test.py
@@ -4,20 +4,20 @@ import fizzbuzz as fb
 
 class TestFizzBuzz(unittest.TestCase):
     def test_fizzBuzz(self):
-        result = fb.fizzbuzz(15)
-        self.assertEqual(result, "FizzBuzz")
+        result = fb.fizzbuzz(15)[1]
+        self.assertEqual(result[1], "FizzBuzz ")
 
     def test_fizz(self):
-        result = fb.fizzbuzz(3)
-        self.assertEqual(result, "Fizz")
+        result = fb.fizzbuzz(3)[1]
+        self.assertEqual(result, "Fizz ")
 
     def test_buzz(self):
-        result = fb.fizzbuzz(5)
-        self.assertEqual(result, "Buzz")
+        result = fb.fizzbuzz(5)[1]
+        self.assertEqual(result, "Buzz ")
 
     def test_int(self):
-        result = fb.fizzbuzz(2)
-        self.assertEqual(result, "2")
+        result = fb.fizzbuzz(2)[1]
+        self.assertEqual(result, "2 ")
 
 if __name__ == '__main__':
     unittest.main()
