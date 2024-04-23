c36b3c12916d1f16ce9968e1c5627ffb338f2956
Nathan Johnson <nathan.johnson@drycreek.me>
2024-04-23 13:58:05 -0600

    Nathan Johnson Assignment 6

---
+++ problems/sumofprimes/java/nathanjohnson/sumprimes2/src/main/java/com/example/sumOfPrimes.java
@@ -0,0 +1,30 @@
+package com.example;
+//import static org.junit.Assert.assertEquals;
+//import org.junit.Test;
+
+public class sumOfPrimes {
+
+    static boolean checkIfPrime(int value) {
+        if(value < 2) {
+                return false;
+        }
+        for (int i = 2; i*2 <= value; i++) {
+                if (value % i == 0) {
+                        return false;
+                }
+        }
+        return true;
+    }
+
+    public static int primeSum(int sumNumber) {
+        int sum = 0;
+        for (int i = 2; i <= sumNumber; i++) {
+                boolean isPrime = checkIfPrime(i); // checks if value i is prime
+                if (isPrime) {
+                        sum = sum + i; // adds i to prime number sum if i is prime
+                }
+        }
+        return sum;
+    }
+
+}
